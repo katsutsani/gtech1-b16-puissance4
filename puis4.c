1c88beb32848725ec97770481e4ef5f2dc286eb2
Firzus <lprieu@gaming.tech>
2021-10-18 15:38:12 +0200

    squelette + swap joueur

--- puis4.c
+++ puis4.c
@@ -2,33 +2,61 @@
 #define NBL 6
 #define NBC 7
 
-// Variables globales:
+// Déclaration variables globales:
 char tab[NBL][NBC]; // OK pour le compilateur car *tout est déterminé* au moment de compiler
 int joueur;         // Joueur 1: =joueur = 0;= et Joueur 1: =joueur = 1;=
 
-
+// INITIALISATION
 
 int init(void)
 {
-  // Texte de bienvenue
+ // Texte de bienvenue
 
   printf("Bienvenue");
+
+ void swap1(int a, int b) // swap joueur 1
+	{
+	  int tmp = a;
+ 	  a = b;
+ 	  b = tmp;
+  	}
+
+ void swap2(int *a, int *b) // swap joueur 2
+	{
+ 	  int tmp = *a;
+ 	  *a = *b;
+ 	  *b = tmp;
+	}
 }
 
+// BOUCLE PRINCIPALE
 
-int main(void) // Boucle principale
+int main(void)
 {
   // Initialisation de la table du jeu
   int table(void){
-    int l,c;
-    // Affichage du plateau
-    for(l=0;l<NBL;l++){
-      printf("\n");
-      for(c=0;c<NBC;c++){
-	tab[l][c] = '.';
-	printf("%s",tab);
-      }
-    }
+  int l,c;
+
+//  void main(void)
+//  {
+  // Swap joueurs
+//  int a=1, b=2;
+//  printf("Début: a = ...\n", ...); // Complétez pour afficher les valeurs des entiers a et b.
+//  swap1(a, b);
+//  printf("Swap1: a = ...\n", ...); // Complétez pour afficher les valeurs des entiers a et b.
+//  swap2(&a, &b);
+//  printf("Swap2: a = ...\n", ...); // Complétez pour afficher les valeurs des entiers a et b.
+//  }
+  // Affichage du plateau
+  for(l=0;l<NBL;l++)
+    {
+    printf("\n");
+    for(c=0;c<NBC;c++)
+    {
+    tab[l][c] = '.';
+	printf("%.1s",tab);
+	}
+}
     printf("\n");
     return 0;
   }
