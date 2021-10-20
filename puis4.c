9194163c15600d045ba34657ca6f39d89930d1ad
Katsutsani <katsutsani@gmail.com>
2021-10-20 08:36:48 +0200

    message bienvenue + start

--- puis4.c
+++ puis4.c
@@ -1,45 +1,55 @@
 #include <stdio.h>
+#include <time.h>
 #define NBL 6
 #define NBC 7
 
 // Déclaration variables globales:
 char tab[NBL][NBC]; // OK pour le compilateur car *tout est déterminé* au moment de compiler
-int joueur;         // Joueur 1: =joueur = 0;= et Joueur 1: =joueur = 1;=
+int joueur; // Joueur 1: =joueur = 0;= et Joueur 1: =joueur = 1;=
+int l,c;
+int start_game;
+
 
 // INITIALISATION
 
-int init(void)
-{
+//int init(void)
 
-}
+
 
 // BOUCLE PRINCIPALE
 
 int main(void)
 {
-  // Texte de bienvenue
-  printf("Bienvenue au jeu du PUISSANCE 4\n");
-  return 0;
+  {
+    while (start_game != 'y')
+      {
+	printf("Welcome to the game of the CONNECT 4\n\nLes dimensions du plateau du jeu sous la forme "<nblignes>x<nbcolonnes>"Start Game ?\n\n[y] - Yes\n[n] - No\n\n"); // Texte de bienvenue + condition de start
+	scanf("%c", &start_game);
+      }
+  }
 
   // Initialisation de la table du jeu
+
   int table(void)
- 	{
-	int l,c;
-	// Affichage du plateau
-	for(l=0;l<NBL;l++){
+  {
+
+    // Affichage du plateau
+
+    for(l=0;l<NBL;l++)
+      {
 	printf("\n");
-	for(c=0;c<NBC;c++){
-	tab[l][c] = '.';
-	printf("%.1s",tab);
+	for(c=0;c<NBC;c++)
+	  {
+	    tab[l][c] = '.';
+	    printf("%.1s",tab);
+	  }
       }
-    }
     printf("\n");
     return 0;
   }
   table();
   return 0;
 }
-
 // Affichage invité (prompteur) pour le joueur courant
 
 // Saisie du numéro de colonne joué par le joueur courant
@@ -47,5 +57,3 @@ int main(void)
 // Actualisation de la table du jeu
 
 // [ Optionel: Test du vainqueur ]
-
-// Changement de joueur (c'est le tour de l'autre joueur)
