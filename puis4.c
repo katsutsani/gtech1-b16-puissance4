992f3ee014d9d0d44fe908bb9749206919b7336c
Katsutsani <katsutsani@gmail.com>
2021-10-20 10:31:21 +0200

    V 0.4 (change table)

--- puis4.c
+++ puis4.c
@@ -8,11 +8,24 @@ char tab[NBL][NBC]; // OK pour le compilateur car *tout est déterminé* au mome
 int joueur; // Joueur 1: =joueur = 0;= et Joueur 1: =joueur = 1;=
 int l,c;
 int start_game;
+char token[] = "ox";
 
 
 // INITIALISATION
 
-//int init(void)
+int init(void){
+  printf("\n\n-------------\n");
+  for(l=0;l<NBL;l++) {
+    for(c=0;c<NBC;c++)   {
+      tab[l][c] = '.';
+      printf("%c ",tab[l][c]);
+    }
+    printf("\n");
+  }
+  printf("\n-------------\n1 2 3 4 5 6 7\n\n");
+  return 0;
+}
+
 
 
 
@@ -20,13 +33,11 @@ int start_game;
 
 int main(void)
 {
-  {
     while (start_game != 'y')
       {
-	printf("Welcome to the game of the CONNECT 4\n\nLes dimensions du plateau du jeu sous la forme "<nblignes>x<nbcolonnes>"Start Game ?\n\n[y] - Yes\n[n] - No\n\n"); // Texte de bienvenue + condition de start
+	printf("Welcome to the game of the CONNECT 4\n\nLes dimensions du plateau du jeu sous la forme : 6x7\n\nStart Game ?\n\n[y] - Yes\n[n] - No\n\n"); // Texte de bienvenue + condition de start
 	scanf("%c", &start_game);
       }
-  }
 
   // Initialisation de la table du jeu
 
@@ -34,21 +45,37 @@ int main(void)
   {
 
     // Affichage du plateau
-
+    printf("\n\n-------------");
     for(l=0;l<NBL;l++)
       {
 	printf("\n");
 	for(c=0;c<NBC;c++)
 	  {
-	    tab[l][c] = '.';
-	    printf("%.1s",tab);
+	    printf("%c ",tab[l][c]);
 	  }
       }
-    printf("\n");
+    printf("\n-------------\n1 2 3 4 5 6 7\n\n");
     return 0;
   }
-  table();
-  return 0;
+  void change_table(int colonne){
+    for(l=(NBL-1);l>=0;l--){
+      for(c=0;c<NBC;c++){
+	if(tab[l][c] == 'x' || tab[l][c] == 'o'){
+	  continue;
+	}
+	else if (tab[l][c] = '.' && c != (colonne-1)){
+	  tab[l][c] = '.';
+	}
+	else{
+	  tab[l][c] = token[0];
+	  colonne = 10;
+	}
+      }
+    }
+    table();
+  }
+  init();
+  change_table(2);
 }
 // Affichage invité (prompteur) pour le joueur courant
 
