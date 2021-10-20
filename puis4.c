6f3593954fb731b263c89455352d90f38b01cb2b
Katsutsani <katsutsani@gmail.com>
2021-10-20 11:34:20 +0200

    V 0.5 (Condition de victoire)

--- puis4.c
+++ puis4.c
@@ -9,6 +9,7 @@ int joueur; // Joueur 1: =joueur = 0;= et Joueur 1: =joueur = 1;=
 int l,c;
 int start_game;
 char token[] = "ox";
+int victory = 0;
 
 
 // INITIALISATION
@@ -31,12 +32,38 @@ int init(void){
 
 // BOUCLE PRINCIPALE
 
+int cond_victoire(void){
+  for(l=0;l<NBL;l++){
+    for(c=0;c<NBC;c++){
+      if(tab[l][c] == tab[l+1][c] && tab[l+1][c] == tab[l+2][c] && tab[l+2][c] == tab[l+3][c] && tab[l][c] != '.')
+	{
+	  printf("Félicitation le joueur %d à gagné la partie",(joueur+1));
+	          victory = 1
+		    }
+      else if(tab[l][c] == tab[l][c+1] && tab[l][c+1] == tab[l][c+2] && tab[l][c+2] == tab[l][c+3] && tab[l][c] != '\
+	      .'){
+	printf("Félicitaion le joueur %d à gagné la partie",(joueur+1));
+	        victory = 1
+		  }
+    }
+  }
+}
+      
+
 int main(void)
 {
     while (start_game != 'y')
       {
 	printf("Welcome to the game of the CONNECT 4\n\nLes dimensions du plateau du jeu sous la forme : 6x7\n\nStart Game ?\n\n[y] - Yes\n[n] - No\n\n"); // Texte de bienvenue + condition de start
 	scanf("%c", &start_game);
+      }
+
+    init()
+      while(victory != 1){
+	if (victory == 2){
+	  printf("Egalité \n");
+	  break;
+	}
       }
 
   // Initialisation de la table du jeu
