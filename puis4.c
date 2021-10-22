46b7dfe1671815b3fd9f97ffd75d39f501ac06f8
Firzus <lprieu@gaming.tech>
2021-10-22 11:06:55 +0200

    V 0.8 (Boucle de jeu, couleur token, condition égalité)

--- puis4.c
+++ puis4.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 #include <time.h>
+#define KNRM "\x1B[0m"
+#define KYEL "\x1B[33m"
+#define KRED "\x1B[31m"
 #define NBL 6
 #define NBC 7
 
 // Déclaration variables globales:
 char tab[NBL][NBC]; // OK pour le compilateur car *tout est déterminé* au moment de compiler
-int joueur; // Joueur 1: =joueur = 0;= et Joueur 1: =joueur = 1;=
+int joueur= 0;// Joueur 1: =joueur = 0;= et Joueur 1: =joueur = 1;=
 int l,c;
 int start_game;
-char token[] = "ox";
+char token[] = "ox"; // -> token[joueur];
 int victory = 0;
 int diago = 0;
+int choix_joueur = 1;
 
 // INITIALISATION
 
@@ -35,10 +39,12 @@ int cond_victoire(void){
         {
           printf("Félicitation le joueur %d à gagné la partie",(joueur+1));
           victory = 1;
+	  choix_joueur=0;
         }
       else if(tab[l][c] == tab[l][c+1] && tab[l][c+1] == tab[l][c+2] && tab[l][c+2] == tab[l][c+3] && tab[l][c] != '.' && c<3){
         printf("Félicitaion le joueur %d à gagné la partie",(joueur+1));
         victory = 1;
+	choix_joueur = 0;
       }
       else  if(l<3 && tab[l][c] == tab[l+1][c+1] && tab[l][c] != '.'){
         for(int i=0;i<3;i++){
@@ -48,6 +54,8 @@ int cond_victoire(void){
         }
         if (diago ==3){
           printf("Victoire");
+	  victory = 1;
+	  choix_joueur=0;
         }
       }
       else if(l>3 && tab[l][c] == tab[l-1][c-1] && tab[l][c] != '.'){
@@ -58,6 +66,8 @@ int cond_victoire(void){
         }
         if(diago == 3){
           printf("Victoire");
+	  victory = 1;
+	  choix_joueur=0;
         }
         else if (l==4 && tab[l][c] == tab[l+1][c+1] && tab[l][c] != '.' || l==4 && tab[l][c] == tab[l-1][c-1] && tab[l][c] != '.'){
           int diago_g = 0;
@@ -72,9 +82,16 @@ int cond_victoire(void){
           }
           if (diago_g == 3 || diago_d ==3){
             printf("victoire");
+	    victory = 1;
+	    choix_joueur = 0;
           }
         }
-        else if(tab[l][c] != '.'){
+        else if(tab[l][c] != '.' && victory == 0){
+	  egalite += 1;
+	  if(egalite = 42){
+	    victory = 2;
+	    choix_joueur = 0;
+	  }
         }
       }
     }
@@ -82,27 +99,19 @@ int cond_victoire(void){
 }
 
 
-int main(void)
+int main(void)   // Boucle principale
 {
   while (start_game != 'y')
     {
-      printf("Welcome to the game of the CONNECT 4\n\nLes dimensions du plateau du jeu sous la forme : 6x7\n\nStart \
-Game ?\n\n[y] - Yes\n[n] - No\n\n"); // Texte de bienvenue + condition de start
+      printf("Welcome to the game of the CONNECT 4\n\nLes dimensions du plateau du jeu sous la forme : 6x7\n\nStart Game ?\n\n[y] - Yes\n[n] - No\n\n"); // Texte de bienvenue + condition de start
       scanf("%c", &start_game);
     }
-
-  // Boucle principale
-  init();
-  while(victory != 1){
-    if (victory == 2){
-      printf("Egalité \n");
-      break;
-    }
-  }
-
-// Initialisation de la table du jeu
-
-  int table(void){
+  
+  
+  // Initialisation de la table du jeu
+  
+  int table(void)
+  {
     // Affichage du plateau
     printf("\n\n-------------");
     for(l=0;l<NBL;l++)
@@ -110,27 +119,64 @@ Game ?\n\n[y] - Yes\n[n] - No\n\n"); // Texte de bienvenue + condition de start
 	printf("\n");
 	for(c=0;c<NBC;c++)
 	  {
-	    printf("%c ",tab[l][c]);
+	    if(tab[l][c] =='x'){
+	      printf("%s%c ",KRED,tab[l][c]);
+	    }
+	    else if(tab[l][c] == 'o'){
+	      printf("%s%c ",KYEL,tab[l][c]);
+	    }
+	    else if (tab[l][c] == '.'){
+	      printf("%s%c ",KNRM,tab[l][c]);
+	    }
 	  }
+      }
     printf("\n-------------\n1 2 3 4 5 6 7\n\n");
     return 0;
-      }
   }
-  void change_table(int colonne){
-    for(l=(NBL-1);l>=0;l--){
-      for(c=0;c<NBC;c++){
-	if(tab[l][c] == 'x' || tab[l][c] == 'o'){
-	  continue;
-	}
-	else if (tab[l][c] == '.' && c != (colonne-1)){
-	  tab[l][c] = '.';
-	}
-	else{
-	  tab[l][c] = token[0];
-	  colonne = 10;
-	}
+  void change_table(int colonne)
+  {
+    for(l=(NBL-1);l>=0;l--)
+      {
+	for(c=0;c<NBC;c++)
+	  {
+	    if(tab[l][c] == 'x' || tab[l][c] == 'o')
+	      {
+		continue;
+	      }
+	    else if (tab[l][c] == '.' && c != (colonne-1))
+	      {
+		tab[l][c] = '.';
+	      }
+	    else
+	      {
+		tab[l][c] = token[joueur];
+		colonne = 10;
+	      }
+	  }
       }
-    }
     table();
   }
+
+  /* boucle de changement de joueur */
+  init();
+  while(victory != 1)
+    {
+      if (victory == 2)
+	{
+	  printf("Egalité \n");
+	  break;
+	}
+      else
+	{
+	  while(choix_joueur>=1&&choix_joueur<=7)
+	    {
+	      printf("En avant joueur %d\n", (joueur+1));
+	      printf("Saisir le numéro de la colonne\n");
+	      scanf("%d",&choix_joueur);
+	      change_table(choix_joueur);
+	      cond_victoire();
+	      joueur = !joueur;
+	    }
+	}
+    }
 }
