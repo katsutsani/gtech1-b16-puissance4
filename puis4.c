ce3c88ae8315a6d094c0b22ef545357ef79e1dce
Katsutsani <katsutsani@gmail.com>
2021-10-27 10:54:01 +0200

    allocation dynamique du tableau

--- puis4.c
+++ puis4.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
-#include <time.h>
+#include <stdlib.h>
 #define KNRM "\x1B[0m"
 #define KYEL "\x1B[33m"
 #define KRED "\x1B[31m"
-#define NBL 6
-#define NBC 7
 
 // Déclaration variables globales:
-char tab[NBL][NBC]; // OK pour le compilateur car *tout est déterminé* au moment de compiler
+char **tab = NULL;
 int joueur= 0;// Joueur 1: =joueur = 0;= et Joueur 1: =joueur = 1;=
-int l,c;
+int l,c,NBL,NBC;
 int start_game;
 char token[] = "ox"; // -> token[joueur];
 int victory = 0;
@@ -18,10 +16,22 @@ int choix_joueur = 0;
 int egalite = 0;
 int column_full = 0;
 
+void flushstdin() {
+  int c;
+  while((c = getchar()) != '\n' && c != EOF);
+}
+
+char **tab_malloc() {
+  char **tab_ = (char**)malloc(NBL * sizeof(char*));
+  for (l = 0; l < NBL; l++)
+    tab_[l] = (char*)malloc(NBC * sizeof(char));
+  return tab_;
+}
+
 // INITIALISATION
 
-int init(void){
-  printf("\n\n-------------\n");
+int init(){
+  printf("\n\n_ _ _ _ _ _ _\n");
   for(l=0;l<NBL;l++) {
     for(c=0;c<NBC;c++)   {
       tab[l][c] = '.';
@@ -29,7 +39,7 @@ int init(void){
     }
     printf("\n");
   }
-  printf("\n-------------\n1 2 3 4 5 6 7\n\n");
+  printf("_ _ _ _ _ _ _\n\n1 2 3 4 5 6 7\n\n");
   return 0;
 }
 
@@ -37,111 +47,123 @@ int cond_victoire(void){
   egalite +=1;
   for (l=0;l<NBL;l++){
     for(c=0;c<NBC;c++){
-      if(tab[l][c] == tab[l+1][c] && tab[l+1][c] == tab[l+2][c] && tab[l+2][c] == tab[l+3][c] && tab[l][c] != '.' && l<4)
+      if(tab[l][c] == tab[l+1][c] && tab[l+1][c] == tab[l+2][c] && tab[l+2][c] == tab[l+3][c] && tab[l][c] != '.' && l<4) //condition de victoire si 4 tokens identiques sont alignés en colonne
         {
           printf("Congratulations player %d, you have won the game !\n",(joueur+1));
           victory = 1;
 	  break;
         }
-      else if(tab[l][c] == tab[l][c+1] && tab[l][c+1] == tab[l][c+2] && tab[l][c+2] == tab[l][c+3] && tab[l][c] != '.' && c<3){
+      else if(tab[l][c] == tab[l][c+1] && tab[l][c+1] == tab[l][c+2] && tab[l][c+2] == tab[l][c+3] && tab[l][c] != '.' && c<3){ //condition de victoire si 4 tokens identiques sont alignés en ligne
         printf("Congratulations player %d, you have won the game !\n",(joueur+1));
         victory = 1;
 	break;
-      }  
-      else if(tab[l][c] == tab[l-1][c+1]&& tab[l-1][c+1] == tab[l-2][c+2] && tab[l-2][c+2] == tab[l-3][c+3] && tab[l][c] != '.'){
+      }
+      else if(tab[l][c] == tab[l-1][c+1] && tab[l-1][c+1] == tab[l-2][c+2] && tab[l-2][c+2] == tab[l-3][c+3] && tab[l][c] != '.'){ //condition de victoire si 4 tokens identiques sont alignés en diagonale
 	printf("Congratulations player %d, you have won the game !\n",(joueur+1));
 	victory = 1;
 	break;
       }
-      else if(tab[l][c] == tab[l+1][c+1] && tab[l+1][c+1] == tab[l+2][c+2] && tab[l+2][c+2] == tab[l+3][c+3] && tab[l][c] != '.'){
+      else if(tab[l][c] == tab[l+1][c+1] && tab[l+1][c+1] == tab[l+2][c+2] && tab[l+2][c+2] == tab[l+3][c+3] && tab[l][c] != '.'){ //condition de victoire si 4 tokens identiques sont alignés en diagonale
 	printf("Congratulations player %d, you have won the game !\n",(joueur+1));
 	victory = 1;
 	break;
       }
     }
   }
-  if(egalite == 42){
+  if(egalite == 42){ //regarde si le plateau est plein
     victory =2;
   }
   choix_joueur = 0;
 }
 
-
-int main(void)   // Boucle principale
+int table(void) //Affichage du plateau
 {
-  while (start_game != 'y')
+  printf("%d %d",NBL,NBC);
+  printf("\n\n- - - - - - -\n");
+  for(l=0;l<NBL;l++)
     {
-      printf("Welcome to the game of the CONNECT 4\n\nLes dimensions du plateau du jeu sous la forme : 6x7\n\nStart Game ?\n\n[y] - Yes\n[n] - No\n\n"); // Texte de bienvenue + condition de start
-      scanf("%c", &start_game);
-    }
-  
-  // Initialisation de la table du jeu
-  
-  int table(void)
-  {
-    // Affichage du plateau
-    printf("\n\n-------------\n");
-    for(l=0;l<NBL;l++)
-      {
-	printf("\n");
-	for(c=0;c<NBC;c++)
-	  {
-	    if(tab[l][c] == 'x'){
-	      printf("%s%c ",KRED, tab[l][c]);
-	    }
-	    else if(tab[l][c] == 'o'){
-	      printf("%s%c ",KYEL,tab[l][c]);
-	    }
-	    else if(tab[l][c] == '.'){
-	      printf("%s%c ",KNRM,tab[l][c]);
-	    }
-	  }
-      }
-    printf("%s\n-------------\n1 2 3 4 5 6 7\n\n",KNRM);
-    return 0;
-  }
-  void change_table(int colonne)
-  {
-    for(l=(NBL-1);l>=0;l--)
-      {
-	for(c=0;c<NBC;c++)
-	  {
-	    if(tab[l][c] == 'x' || tab[l][c] == 'o')
-	      {
-		continue;
-	      }
-	    else if (tab[l][c] == '.' && c != (colonne-1))
-	      {
-		tab[l][c] = '.';
-	      }
-	    else
-	      {
-		tab[l][c] = token[joueur];
-		colonne = 10;
-	      }
-	  }
-      }
-    table();
-  }
-
-  /* boucle de changement de joueur */
-  init();
-  while(victory != 1)
-    {
-      while(choix_joueur<1 || choix_joueur>7)
+      printf("\n");
+      for(c=0;c<NBC;c++)
 	{
-	  printf("Your turn player number %d, enter the column number :\n\n", (joueur+1));
-	  scanf("%d",&choix_joueur);
+	  if(tab[l][c] == 'x'){
+	    printf("%s%c ",KRED, tab[l][c]);
+	  }
+	  else if(tab[l][c] == 'o'){
+	    printf("%s%c ",KYEL,tab[l][c]);
+	  }
+	  else if(tab[l][c] == '.'){
+	    printf("%s%c ",KNRM,tab[l][c]);
+	  }
 	}
-      if(choix_joueur){
+    }
+  printf("%s\n- - - - - - -\n1 2 3 4 5 6 7\n\n",KNRM);
+  return 0;
+}
+
+void change_table(int colonne) //changer les tableau pour entrer le token
+{
+  printf("%d %d",NBL,NBC);
+  for(l=(NBL-1);l>=0;l--)
+    {
+      for(c=0;c<NBC;c++)
+	{
+	  if(tab[l][c] == 'x' || tab[l][c] == 'o')
+	    {
+	      continue;
+	    }
+	  else if (tab[l][c] == '.' && c != (colonne-1))
+	    {
+	      tab[l][c] = '.';
+	    }
+	  else
+	    {
+	      tab[l][c] = token[joueur];
+	      colonne = 10;
+	    }
+	}
+    }
+  table();
+}
+
+
+ int main(int argc, char *argv[])   // Boucle principale
+{
+  if (argc == 3){
+    NBL = atoi(argv[1]);
+    NBC = atoi(argv[2]);
+  } else {
+    NBL = 6;
+    NBC = 7;
+  }
+  tab = tab_malloc();
+  if(tab != NULL){
+    while (start_game != 'y')
+      {
+	printf("%d \n",NBC);
+	printf("Welcome to the game of the CONNECT 4\n\nLes dimensions du plateau du jeu sous la forme : 6x7\n\nStart Game ?\n\n[y] - Yes\n[n] - No\n\n"); // Texte de bienvenue + condition de start
+	scanf("%c", &start_game);
+      }
+    
+    // boucle de changement de joueur
+    init();
+    while(victory != 1)
+      {
+	while(choix_joueur<1 || choix_joueur>7)
+	  {
+	    printf("Your turn player number %d, your token is [%c], enter the column number :\n\n", (joueur+1),token[joueur]);
+	    int retour_scanf = scanf("%d",&choix_joueur);
+	    if(retour_scanf != 1){
+	      flushstdin();
+	    }
+	  }
 	column_full = 0;
 	for(l=0;l<NBL;l++){
 	  if(tab[l][choix_joueur-1] == 'x' || tab[l][choix_joueur-1] == 'o'){
 	    column_full +=1;
 	  }
 	}
-	if(column_full == 6){
-	  printf("The column selected is already full, please select another one\n");
+	if(column_full == 6){ //test si la colonne choisie est pleine
+	  printf("The column selected is already full, please select another one\n \n");
 	  column_full = 0;
 	  choix_joueur = 0;
 	}
@@ -156,9 +178,8 @@ int main(void)   // Boucle principale
 	    joueur = !joueur;
 	  }
 	}
-      }     
-      else{
-	break;
       }
-    }
+    free(tab);
+    return 0;
+  }
 }
